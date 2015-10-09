98d6992b5a31f05776248411cc9ca61bc365c157
Zach Panzarino <zachary@panzarino.com>
2015-10-09 23:44:55 +0000

    Points issues fixed

--- football/points.py
+++ football/points.py
@@ -2,12 +2,21 @@ import nflgame # https://github.com/BurntSushi/nflgame
 from football import stats, bye_week
 
 def get_stats(name, year, wks):
+    # gets stats for a player
+    # note: weeks must be defined as a list
     player = nflgame.find(name, team=None)[0]
     byeweek = bye_week.find(player.team, year)
     player_stats = {}
     for x in range(0, len(wks)):
         dictprop = 'week_'+str(wks[x])
+        player_stats[dictprop]=(stats.player_stats(name, year, wks[x]))
         if wks[x] == byeweek:
-            player_stats[dictprop]=None
+            player_stats[dictprop]['bye_week'] = True
         else:
-            player_stats[dictprop]=(stats.player_stats(name, year, wks[x]))
+            player_stats[dictprop]['bye_week'] = False
+    return player_stats
+
+def standard_player_points(name, year, wks):
+    player_stats = get_stats(name, year, wks)
+    points = {}
+    # unfinished method
