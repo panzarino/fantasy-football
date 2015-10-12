287c85c66a388d3d2d9c6d305a9f7241e0089c45
Zach Panzarino <zachary@panzarino.com>
2015-10-12 17:31:21 +0000

    Standard Scoring

--- football/points.py
+++ football/points.py
@@ -17,6 +17,25 @@ def get_stats(name, year, wks):
     return player_stats
 
 def standard_player_points(name, year, wks):
+    # returns standard scoring points
     player_stats = get_stats(name, year, wks)
+    print player_stats
     points = {}
-    # unfinished method
+    for x in player_stats:
+        points[x]={}
+    for x in player_stats:
+        points[x]['rushing_yds']=int(player_stats[x]['rushing_yds']/10)
+        points[x]['rushing_tds']=player_stats[x]['rushing_tds']*6
+        points[x]['receiving_yds']=int(player_stats[x]['receiving_yds']/10)
+        points[x]['receiving_tds']=player_stats[x]['receiving_tds']*6
+        points[x]['fumbles']=player_stats[x]['fumbles']*(-2)
+        points[x]['puntret_tds']=player_stats[x]['puntret_tds']*6
+        points[x]['kickret_tds']=player_stats[x]['kickret_tds']*6
+        points[x]['rushing_2pt']=player_stats[x]['rushing_2pt']*2
+        points[x]['receiving_2pt']=player_stats[x]['receiving_2pt']*2
+        points[x]['passing_yds']=int(player_stats[x]['passing_yds']/25)
+        points[x]['passing_tds']=player_stats[x]['passing_tds']*4
+        points[x]['passing_ints']=player_stats[x]['passing_ints']*(-2)
+        points[x]['passing_2pt']=player_stats[x]['passing_2pt']*2
+        points[x]['bye_week']=player_stats[x]['bye_week']
+    return points
