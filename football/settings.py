c3b1ff889c5bbb62fb77c637bf2ad4798aa8f9d7
Zach Panzarino <zachary@panzarino.com>
2015-10-13 17:31:17 +0000

    Template_Dirs

--- football/settings.py
+++ football/settings.py
@@ -82,3 +82,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os.path
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'html'),
+)
